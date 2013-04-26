5e05eaea2ece9bc7f3dcc747afc6dcfec2bf3ef6
Guillermo Guerini <guillermo@gguerini.com>
2013-04-26 11:28:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UscisWeb::Application.config.secret_token = 'cc18bb338b077569016bf3bc272dc5e24014c0a5f597a0afa7374b89047294813c846c82172ca7e0d000c8bb2b1427bec578f5fb23a59c32791031d206215c1d'
