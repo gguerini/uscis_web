60ecba774c913fdb05edd46a350a171b97316452
Guillermo Guerini <guillermo@gguerini.com>
2013-05-01 11:47:16 -0700

    Add authentication and registration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 UscisWeb::Application.routes.draw do
 
+  devise_for :users
+  devise_scope :user do
+    get "sign_in", :to => "devise/sessions#new"
+    get "sign_up", :to => "devise/registrations#new"
+    get "sign_out", :to => "devise/sessions#destroy", via: :delete
+  end
+
   root :to => 'home#index'
 
 end
